5df41f56aa3c5421531809aa0b9fc97c3d90a011
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2017-01-10 23:53:27 +0100

    adding api.py with empty sor function

---
+++ pysor/api.py
@@ -0,0 +1,20 @@
+#   PySOR - solve Poisson's equation with successive over-relaxation.
+#   Copyright (C) 2017  Christoph Wehmeyer
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+
+def sor(rho, h, epsilon=1.0, maxiter=1000, maxerr=1.0E-7, w=None):
+    pass

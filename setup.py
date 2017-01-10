36cce0a6e727092cd232942668201c86baf2584b
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2017-01-10 17:15:07 +0100

    added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#   PySOR - solve Poisson's equation with successive over-relaxation.
+#   Copyright (C) 2017  Christoph Wehmeyer
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+
+setup(
+    name='pysor',
+    version='0.0.0',
+    description="Solve Poisson's equation with successive over-relaxation",
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics'],
+    keywords=[],
+    url='https://github.com/cwehmeyer/pysor',
+    author='Christoph Wehmeyer',
+    author_email='christoph.wehmeyer@fu-berlin.de',
+    license='GPLv3+',
+    packages=['pysor'])
